6fd668660cb03d15ef5e1754620ae1050b947e87
alejandro <alejandroriveros100@gmail.com>
2024-07-16 19:12:07 -0500

    [add] api literatura project

---
+++ api_literatura/src/main/java/com/alejandro/api_literatura/ApiLiteraturaApplication.java
@@ -0,0 +1,13 @@
+package com.alejandro.api_literatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLiteraturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLiteraturaApplication.class, args);
+	}
+
+}
---
+++ api_literatura/src/test/java/com/alejandro/api_literatura/ApiLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandro.api_literatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
